76f224d15f07fe9f0bc52f15982d3b4758b545a9
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-10-02 20:52:51 -0300

    config: UTC Timezone

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class Post(Base):
     ts = Column(BigInteger, primary_key=True)
     title = Column(String, nullable=False)
     has_label = Column(Boolean, nullable=False)
-    created_at = Column(DateTime, default=datetime.now(timezone.now))
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
 
     def __repr__(self):
         return f"<Post(ts={self.ts}, title='{self.title}', has_label='{self.has_label}', created_at={self.created_at})>"
