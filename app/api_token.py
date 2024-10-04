ee0a104d8015a6dc1eeb3a6f44d811e5760d4a00
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-10-04 18:03:03 -0300

    update: get_token_access

--- app/api_token.py
+++ app/api_token.py
@@ -11,8 +11,15 @@ def get_token_access(headers, params):
     try:
         auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
         response = requests.post('https://www.reddit.com/api/v1/access_token', auth=auth, data=params, headers=headers)
+
+        if response.status_code != 200:
+            raise APITokenError(f"Falha ao capturar token de acesso: {response.text}")
+
+        token = response.json().get('access_token')
+        if not token:
+            raise APITokenError("Token de acesso não encontrado na resposta.")
+
+        return token
+
     except Exception as e:
-        raise APITokenError(f"Falha ao capturar token de acesso: {e}")
-    else:
-        token = response.json()['access_token']
-    return token
+        raise APITokenError(f"Erro ao capturar token de acesso: {e}")
