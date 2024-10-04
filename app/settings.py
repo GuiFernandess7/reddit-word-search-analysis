a41b6f01c951566f1139bd187bf1a8d4c5a2391c
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-10-04 17:30:34 -0300

    feat: Initial params added

--- app/settings.py
+++ app/settings.py
@@ -2,9 +2,15 @@ import dotenv
 import os
 dotenv.load_dotenv()
 
-#CLIENT_ID = os.getenv('CLIENT_ID')
-#CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-#USERNAME = os.getenv('USERNAME')
-#PASSWORD = os.getenv('PASSWORD')
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+USERNAME = os.getenv('USERNAME')
+PASSWORD = os.getenv('PASSWORD')
 USER_AGENT = os.getenv('USER_AGENT')
 API_TOKEN = os.getenv('API_TOKEN')
+
+INITIAL_PARAMS = {
+    'grant_type': 'password',
+    'username': USERNAME,
+    'password': PASSWORD
+}
