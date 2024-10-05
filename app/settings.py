1af7c32d9eabf6eb1d5b44f81c108eff931c3a5a
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-10-04 21:30:57 -0300

    config: debug option updated

--- app/settings.py
+++ app/settings.py
@@ -13,9 +13,9 @@ USER_AGENT = os.getenv('USER_AGENT')
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
 
 if not DEBUG:
-    SERVICE_ACCOUNT_FILE = os.path.join(os.path.dirname(__file__), '.creds', 'creds.json')
-else:
     SERVICE_ACCOUNT_FILE = os.getenv('SERVICE_ACCOUNT_FILE')
+else:
+    SERVICE_ACCOUNT_FILE = os.path.join(os.path.dirname(__file__), '.creds', 'creds.json')
 
 FOLDER_ID = "1iedDsGWCMNFQ6BlUdMLtZBVwzsMLW9dY"
 
