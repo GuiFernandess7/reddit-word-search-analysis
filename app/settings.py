3236ff2d74358b81e662b37221546d2adc8198f4
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-10-04 21:26:10 -0300

    config: google drive var envs

--- app/settings.py
+++ app/settings.py
@@ -8,6 +8,10 @@ USERNAME = os.getenv('USERNAME')
 PASSWORD = os.getenv('PASSWORD')
 USER_AGENT = os.getenv('USER_AGENT')
 
+SCOPES = ['https://www.googleapis.com/auth/drive.file']
+SERVICE_ACCOUNT_FILE = os.path.join(os.path.dirname(__file__), '.creds', 'creds.json')
+FOLDER_ID = "1iedDsGWCMNFQ6BlUdMLtZBVwzsMLW9dY"
+
 INITIAL_PARAMS = {
     'grant_type': 'password',
     'username': USERNAME,
